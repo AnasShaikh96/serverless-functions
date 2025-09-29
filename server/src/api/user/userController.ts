daa1cbedc56a6645ebcbb6ada9318e7503dc26db
Anas Shaikh <shailesh@kinemeric.com>
2025-09-29 18:05:31 +0530

    convert class user controller to function

--- server/src/api/user/userController.ts
+++ server/src/api/user/userController.ts
@@ -1,16 +1,16 @@
 import type { Request, RequestHandler, Response } from "express";
 
-import { userService } from "@/api/user/userService";
+import { findAll, findById } from "@/api/user/userService";
 
 class UserController {
 	public getUsers: RequestHandler = async (_req: Request, res: Response) => {
-		const serviceResponse = await userService.findAll();
+		const serviceResponse = await findAll();
 		res.status(serviceResponse.statusCode).send(serviceResponse);
 	};
 
 	public getUser: RequestHandler = async (req: Request, res: Response) => {
 		const id = Number.parseInt(req.params.id as string, 10);
-		const serviceResponse = await userService.findById(id);
+		const serviceResponse = await findById(id);
 		res.status(serviceResponse.statusCode).send(serviceResponse);
 	};
 }
