74508bfccf0f97ce2e5ab1e44c86aa3dfaa39f95
Anas Shaikh <shaikhanas246@gmail.com>
2025-09-30 22:41:17 +0530

    wip function model

---
+++ server/src/api/functions/functionModel.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export type FunctionType = z.infer<typeof functionSchema>;
+
+export const functionSchema = z.object({
+  type: z.enum(["http", "schedule", "cmd"]),
+  runtime: z.enum(["Node.18", "Node.20"]),
+  requestType: z.enum(["GET", "POST"]),
+});
