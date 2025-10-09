b21bec0cad195fe6f601ded6ec4190f0ab98a410
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-09 13:06:17 +0530

    feat: polling lifecycle complete

--- other-server/polling-worker.js
+++ other-server/polling-worker.js
@@ -19,38 +19,73 @@ async function poll() {
 	try {
 		const res = await axios.get(`${BASE_URL}/invocation/next`);
 		if (res.status === 204) {
+			console.log("polling")
 			// No job — wait & poll again
 			await new Promise((r) => setTimeout(r, 2000));
 			return poll();
 		}
 
-		const job = JSON.parse(res.data);
-		const id = job.job_id;
-		const fnid = job.fn_id
-		console.log("[Poller] Received job:", job);
+		// const job = JSON.parse(res.data);
+		const job = res.data;
+		const jobId = job.id
+
+		console.log("job received", job)
+
+
+
+		// const jobArr = Object.values(job).flat()
+
+		// console.log("[Poller] Received job:", jobArr);
+
 
 		try {
-			const output = await executeJob(Object.values(job));
+			const output = await executeJob(job);
 
-
-
-			await axios.post(`${BASE_URL}/invocation/response/${fnid}/${id}`, {
-				jobId: job.id,
-				output,
+			await axios.post(`${BASE_URL}/invocation/response`, {
+				jobId: jobId, output
 			});
 			console.log("[Poller] ✅ Sent success response");
 		} catch (err) {
-			await axios.post(`${BASE_URL}/invocation/error/${fnid}/${id}`, {
-				jobId: job.id,
-				error: err.message,
+			await axios.post(`${BASE_URL}/invocation/error`, {
+				jobId: jobId, error: err
 			});
 			console.log("[Poller] ❌ Sent error response");
 		}
 
+
+		// jobArr.forEach(async jobItem => {
+
+		// 	const id = jobItem.job_id;
+		// 	const fnid = jobItem.fn_id
+		// 	try {
+		// 		const output = await executeJob(jobItem);
+
+
+
+		// 		await axios.post(`${BASE_URL}/invocation/response/${fnid}/${id}`, {
+		// 			jobId: id,
+		// 			output,
+		// 		});
+		// 		console.log("[Poller] ✅ Sent success response");
+		// 	} catch (err) {
+		// 		await axios.post(`${BASE_URL}/invocation/error/${fnid}/${id}`, {
+		// 			jobId: id,
+		// 			error: err.message,
+		// 		});
+		// 		console.log("[Poller] ❌ Sent error response");
+		// 	}
+		// })
+
+
+
+
+
+
+
 		// Immediately continue polling
 		poll();
 	} catch (err) {
-		console.error("[Poller] Poll error:", err.message);
+		// console.error("[Poller] Poll error:", err);
 		// retry after delay
 		setTimeout(poll, 3000);
 	}
--- other-server/polling.js
+++ other-server/polling.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
+const { v4: uuid4 } = require('uuid')
+
 
 let pendingJob = null;
 
-let jobQueue = new Map();
+let jobQueue = [];
+let pendingPromises = new Map();
 
 
 // const obj = {
@@ -33,31 +36,67 @@ app.get('/add-jobs/:id/:fn', async (req, res) => {
 
     const fnId = req.params.id
     const fnName = req.params.fn
+    const jobId = uuid4()
 
 
-    if (jobQueue.get(fnId) !== undefined) {
-        jobQueue.set(fnId, [...jobQueue.get(fnId), {
-            job_id: Math.random() * 10000,
-            fn_id: fnId,
-            fn_name: fnName
-        }])
-    } else {
-        jobQueue.set(fnId, [{
-            job_id: Math.random() * 10000,
-            fn_id: fnId,
-            fn_name: fnName
-        }])
-    }
+    jobQueue.push({ id: jobId, payload: { fnId, fnName } })
 
-    res.status(200).json({
-        jobCount: Object.fromEntries(jobQueue),
-        message: 'Job Added successfully!'
+
+    const result = await new Promise((resolve, reject) => {
+        pendingPromises.set(jobId, { resolve, reject })
+        setTimeout(() => {
+            if (pendingPromises.has(jobId)) {
+                pendingPromises.delete(jobId);
+                reject(new Error('Job Timeout'))
+            }
+        }, 15000);
+
     })
 
 
 
+    console.log("result", await result)
+    res.json({
+        message: "inside",
+        result
+    })
 
 
+    // jobQueue.set(jobId, Promise {
+    //     status: 'pending',
+    //     jobId,
+    //     fnId,
+    //     fnName
+    // })
+
+
+
+    // console.log()
+    // res.send({
+    //     jobId,
+    //     fnId, fnName
+    // })
+
+
+
+    // if (jobQueue.get(fnId) !== undefined) {
+    //     jobQueue.set(fnId, [...jobQueue.get(fnId), {
+    //         job_id: Math.random() * 10000,
+    //         fn_id: fnId,
+    //         fn_name: fnName
+    //     }])
+    // } else {
+    //     jobQueue.set(fnId, [{
+    //         job_id: Math.random() * 10000,
+    //         fn_id: fnId,
+    //         fn_name: fnName
+    //     }])
+    // }
+
+    // res.status(200).json({
+    //     jobCount: Object.fromEntries(jobQueue),
+    //     message: 'Job Added successfully!'
+    // })
 
 
     // pendingJob = { id: Date.now(), payload: { number: 204 } };
@@ -74,15 +113,24 @@ app.get('/add-jobs/:id/:fn', async (req, res) => {
 // Route: Node poller hits this repeatedly
 app.get("/invocation/next", (req, res) => {
 
+    if (jobQueue.length > 0) {
+        const nextJob = jobQueue.shift();
+        res.send(nextJob)
 
-    if (jobQueue.size !== 0) {
-
-        const plainObject = Object.fromEntries(jobQueue);
-        const jsonString = JSON.stringify(plainObject)
-        res.json(jsonString)
     } else {
-        res.status(204).send() //No Job if Map size is 0
+        res.status(204).send() // No Job in queue
     }
+
+
+
+    // if (jobQueue.size !== 0) {
+
+    //     const plainObject = Object.fromEntries(jobQueue);
+    //     const jsonString = JSON.stringify(plainObject)
+    //     res.json(jsonString)
+    // } else {
+    //     res.status(204).send() //No Job if Map size is 0
+    // }
 
 
 
@@ -100,25 +148,59 @@ app.get("/invocation/next", (req, res) => {
 });
 
 // Response endpoints
-app.post("/invocation/response/:fnid/:jobid", (req, res) => {
+app.post("/invocation/response", (req, res) => {
 
-    const jobId = req.params.jobid;
-    const fnId = req.params.fnid
+    const { jobId, output } = req.body;
 
-    const clearJob = jobQueue.get(fnId)
+    const pending = pendingPromises.get(jobId)
+
+
+    if (pending) {
+        pending.resolve({ status: 'success', output })
+        pendingPromises.delete(jobId)
+    }
+
+    res.sendStatus(200)
+    // res.status(200).json({
+    //     sss: 'sss'
+    // })
+
+    // const fnId = req.params.fnid
+
+
+
+    // const clearJob = jobQueue.get(fnId)
     // const removeJob = clearJob.filter(id => id.job_id !== jobId);
     // jobQueue.set(removeJob)
 
-    console.log(clearJob, fnId, jobQueue)
+    // console.log(clearJob, fnId, jobQueue)
 
-    console.log("[JobServer] ✅ Success:", req.body);
-    res.sendStatus(200);
-    jobQueue.clear()
+    // console.log("[JobServer] ✅ Success:", req.body);
+    // res.sendStatus(200);
+    // jobQueue.clear()
+
+
+
 });
 
-app.post("/invocation/error/:fnid/:jobid", (req, res) => {
-    console.log("[JobServer] ❌ Error:", req.body);
-    res.sendStatus(200);
+app.post("/invocation/error", (req, res) => {
+
+    const { jobId, error } = req.body
+    const pending = pendingPromises.get(jobId);
+
+
+
+    if (pending) {
+        pending.reject({ status: 'reject', error })
+        pendingPromises.delete(jobId)
+    }
+
+
+    res.sendStatus(200)
+
+
+    // console.log("[JobServer] ❌ Error:", req.body);
+    // res.sendStatus(200);
 });
 
 app.listen(9001, () => console.log("[JobServer] Listening on port 9001"));
