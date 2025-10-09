25f865d8621d04035e2e295e25cb0db293b4b35d
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-09 10:33:18 +0530

    feat: add Job poller and polling workers

--- other-server/polling.js
+++ other-server/polling.js
@@ -1,16 +1,48 @@
-const express = require("express")
-const cors = require("cors")
-const fs = require("fs")
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+let pendingJob = null;
+
+// Simulate job creation every 10 seconds
+// setInterval(() => {
+//   pendingJob = { id: Date.now(), payload: { number: Math.floor(Math.random() * 10) } };
+//   console.log("[JobServer] New job available:", pendingJob);
+// }, 5000);
 
 
+app.get('/add-jobs', async (req, res) => {
 
-app.use('/invoke/next')
+    pendingJob = { id: Date.now(), payload: { number: 204 } };
+    console.log("pendingJob", pendingJob)
+
+    res.status(200).json({
+        jobCount: pendingJob,
+        message: 'Job Added successfully!'
+    })
+})
 
 
-app.use('/invoke/:id/response')
-app.use('/invoke/:id/error')
+// Route: Node poller hits this repeatedly
+app.get("/invocation/next", (req, res) => {
+    if (pendingJob) {
+        res.json(pendingJob);
+        pendingJob = null; // mark as taken
+    } else {
+        res.status(204).send(); // no job yet
+    }
+});
+
+// Response endpoints
+app.post("/invocation/response", (req, res) => {
+    console.log("[JobServer] ✅ Success:", req.body);
+    res.sendStatus(200);
+});
+
+app.post("/invocation/error", (req, res) => {
+    console.log("[JobServer] ❌ Error:", req.body);
+    res.sendStatus(200);
+});
+
+app.listen(9001, () => console.log("[JobServer] Listening on port 9001"));
