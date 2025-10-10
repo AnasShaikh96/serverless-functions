92f4876498247993cf9cc8e32f91c8ec30c9db64
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-10 20:24:41 +0530

    init: regional server to mock fn lifecycle

--- regional-server/src/index.ts
+++ regional-server/src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import cors from "cors";
 import fs from "fs";
 import extractArchive from "./utils/extractArchieve.ts";
+import { makeDockerFile } from "./utils/makeDockerfile.ts";
+import { exec } from "child_process";
+import { composeText } from "./utils/composeText.ts";
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -39,45 +43,79 @@ app.use("/user-bucket/:id", (req, res) => {
   });
 });
 
-app.use("/store-function/:id", async (req, res) => {
+app.use("/store-function", async (req, res) => {
   const path = req.body;
-  const createDir = `./src/function-buckets/${req.params.id}`;
+  const id = req.body;
+  const nodeV = req.body;
+
+  const createDir = `./src/function-buckets/${id}`;
+
   const pathUrl = await extractArchive(
     path.fileUrl,
     createDir,
     "helloDynamic.js"
   );
 
+  const dockerFiletext = makeDockerFile(nodeV);
+
+  fs.writeFile(createDir + "/Dockerfile", dockerFiletext, (err) => {
+    if (err) {
+      console.log("error while creating Dockerfile", err);
+    }
+  });
+
+  fs.writeFile(createDir + "/compose.yaml", composeText, (err) => {
+    console.log("error while creating compose file", err);
+  });
+
+  const dockerCMd = `cd ${createDir} && npm init -y  && npm i && docker compose up --build`;
+
+  exec(dockerCMd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send(`Error starting container: ${error.message}`);
+    }
+    console.log(`Container started: ${stdout}`);
+
+    // You would add logic here to verify the container is truly "up"
+    // For example, by probing a health endpoint on the newly started container,
+    // or waiting for a specific log message.
+    // For simplicity, we'll assume it's up after the 'docker run' command.
+    res.send("OK: Container is starting/started.");
+  });
+
   const fullUrl = req.protocol + "://" + req.get("host");
 
   res.status(200).json({
-    functionUrl: `${fullUrl}/invoke-function/${req.params.id}${pathUrl?.replace(
+    functionUrl: `${fullUrl}/invoke-function/${id}${pathUrl?.replace(
       createDir,
       ""
     )}`,
   });
 });
 
-app.use("/invoke-function/:id/:fnname", async (req, res) => {
-  const reqMethod = req;
 
-  const query = req.params;
-  const fileName = req.params.fnname;
-  const fnName = fileName.replace(".js", "");
+// old method
+// app.use("/invoke-function/:id/:fnname", async (req, res) => {
+//   const reqMethod = req;
 
-  switch (reqMethod.method) {
-    case "GET":
-      const moduel = require(`./src/function-buckets/${query.id}/${fileName}`);
+//   const query = req.params;
+//   const fileName = req.params.fnname;
+//   const fnName = fileName.replace(".js", "");
 
-      console.log(await moduel[`${fnName}`]);
-      await moduel[`${fnName}`](res);
-    default:
-      res.status(200).json({
-        message: "Invalid method",
-      });
-  }
-});
+//   switch (reqMethod.method) {
+//     case "GET":
+//       const moduel = require(`./src/function-buckets/${query.id}/${fileName}`);
 
-app.listen(3000, () => {
-  console.log("server up at 3000");
+//       console.log(await moduel[`${fnName}`]);
+//       await moduel[`${fnName}`](res);
+//     default:
+//       res.status(200).json({
+//         message: "Invalid method",
+//       });
+//   }
+// });
+
+app.listen(port, () => {
+  console.log(`server up at ${port}`);
 });
