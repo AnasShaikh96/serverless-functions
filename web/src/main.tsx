be911e0daf95c639c1e8c7f358c8a47a7fc60275
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-15 11:48:14 +0530

    add monaco editor for code editing

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-
+// import App from './App.tsx'
+import { Editor } from './components/editor.tsx'
+import './userworker'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Editor />
+    {/* <App /> */}
   </StrictMode>,
 )
