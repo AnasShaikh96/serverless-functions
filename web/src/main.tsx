6ec0ab82caf25cc56e4212f2e5e6288169fb805a
Anas Shaikh <shaikhanas246@gmail.com>
2025-10-15 14:55:57 +0530

    add monaco react plugin and code blocks

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
-import { Editor } from './components/editor.tsx'
+// import { Editor } from './components/editor.tsx'
 import './userworker'
+import { Editor } from '@monaco-editor/react'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Editor />
+    {/* <Editor /> */}
     {/* <App /> */}
   </StrictMode>,
 )
